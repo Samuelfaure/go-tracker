145ba513a0db4ec6a798859aafb50b537eca4ad2
Samuelfaure <samuel.faure.dev@gmail.com>
2019-11-30 17:01:36 +0100

    Boyscout - cleaning

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,79 +1,78 @@
 package tracker
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
 )
 
 type (
-	ChannelChanges chan int
+	ChanChanges chan int
 
 	TrackerContext struct {
 		echo.Context
-		ChannelChanges
+		ChanChanges
 	}
 )
 
 func Init() {
-	visitors := 0
 	changes := make(chan int)
 
-	go count(&visitors, changes)
+	go count(changes)
+	startServer(changes)
+}
 
-	// Creating server
+func startServer(changes ChanChanges) {
 	e := echo.New()
+	registerMiddlewares(e, changes)
 
-	// Middlewares
+	e.GET("/ws/:token", func(c echo.Context) error {
+		tc := c.(*TrackerContext)
+		tc.StartWebsocket(c)
+		return tc.String(200, "OK")
+	})
+
+	e.Start(":1323")
+}
+
+func registerMiddlewares(e *echo.Echo, changes ChanChanges) {
 	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			tc := &TrackerContext{c, changes}
 			return h(tc)
 		}
 	})
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/ws/:msg", func(c echo.Context) error {
-		tc := c.(*TrackerContext)
-		tc.StartWebsocket(c)
-		return tc.String(200, "OK")
-	})
-
-	// Manage closing
-	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func (tc *TrackerContext) StartWebsocket(c echo.Context) {
-	changes := tc.ChannelChanges
+	changes := tc.ChanChanges
 	handleOpen(changes)
-
 	websocket.Handler(func(ws *websocket.Conn) {
-		// Rules for closing and opening
-		defer handleClose(changes)
 		defer ws.Close()
 
-		// Websocket
 		for {
-			err := websocket.Message.Send(ws, "Connection ON")
-			if err != nil {
+			if err := websocket.Message.Send(ws, "Connection ON"); err != nil {
 				break
 			}
 
-			msg := c.Param("msg") // TODO replace with a token, break if incorrect
-			err = websocket.Message.Receive(ws, &msg)
-			if err != nil {
+			token := c.Param("token") // TODO decrypt / check token
+			if err := websocket.Message.Receive(ws, &token); err != nil {
 				break
 			}
 		}
 	}).ServeHTTP(c.Response(), c.Request())
+	handleClose(changes)
 }
 
-func count(visitors *int, changes chan int) {
+func count(changes chan int) {
+	visitors := 0
+
 	for {
 		change := <-changes
-		*visitors += change
+		visitors += change
+		fmt.Printf("count: %d\n", visitors)
 		// TODO: Send to kafka
 	}
 }
